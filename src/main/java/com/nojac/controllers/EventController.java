f054ccd1c50d78c9e8becc5449a01f6327c2e251
masterchief66 <www.napoleontot@gmail.com>
2017-05-31 20:21:22 +0300

    find a good time

--- src/main/java/com/nojac/controllers/EventController.java
+++ src/main/java/com/nojac/controllers/EventController.java
@@ -1,6 +1,8 @@
 package com.nojac.controllers;
 
 import com.nojac.errors.BadDateFormatException;
+import com.nojac.models.Attendant;
+import com.nojac.models.Calendar;
 import com.nojac.models.Event;
 import com.nojac.models.NjUser;
 import com.nojac.repositories.EventRepository;
@@ -8,18 +10,19 @@ import com.nojac.repositories.NjUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by nickolas on 5/30/17.
  */
 @RestController
 public class EventController {
+
+    private long beginTime;
+    private long endTime;
 
     @Autowired
     private EventRepository eventRepository;
@@ -63,6 +66,58 @@ public class EventController {
         Set<Event> events = njUser.getCalendar().getEvents();
         return events;
     }
+
+
+    public Set<Date> findAvailability(Event event,String D1,String D2){
+        //gather all calendars associated with this event
+        Calendar ownerCalendar = event.getCalendar();
+        Set<Attendant> attendants = event.getAttendants();
+        //the available dates
+        Set<Date> rDates = new HashSet<>();
+
+        Calendar temp = new Calendar();
+        for(Attendant att: attendants) {
+            for (Event ev : att.getEvents()) {
+                temp.getEvents().add(ev);
+            }
+        }
+        for (Event ev : ownerCalendar.getEvents()){
+            temp.getEvents().add(ev);
+        }
+
+        //the time is set by the organizer
+        setUp(D1,D2);
+        //creating datetimes..
+        for(int i=1;i<250;i++) rDates.add(new Date(getRandomTimeBetweenTwoDates()));
+        //check which dates are valid
+
+        for(Event ev: temp.getEvents()){
+            for(Date d: rDates) {
+                if ((d.before(ev.getEnd()) &&  d.after(ev.getStart())) || d.equals(ev.getEnd()) || d.equals(ev.getStart()))
+                   rDates.remove(d);
+            }
+        }
+        return rDates;
+    }
+
+
+    private void setUp (String D1 ,String D2) {
+        //D1 = "2013-01-01 00:00:00"
+        //D2 = "2013-12-31 00:58:00"
+        beginTime = Timestamp.valueOf(D1).getTime();
+        endTime = Timestamp.valueOf(D2).getTime();
+    }
+
+    private long getRandomTimeBetweenTwoDates () {
+        long diff = endTime - beginTime + 1;
+        return beginTime + (long) (Math.random() * diff);
+    }
+
+
+
+
+
+
 
 
     @RequestMapping(value="/event", method=RequestMethod.PATCH)
