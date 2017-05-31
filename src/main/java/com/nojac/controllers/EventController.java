cda3aa8abd69665a26fd47ba96d9d3d979c77de6
nikos <nikos@psycholate.com>
2017-05-31 20:23:09 +0300

    "Massive" improvements

--- src/main/java/com/nojac/controllers/EventController.java
+++ src/main/java/com/nojac/controllers/EventController.java
@@ -41,6 +41,39 @@ public class EventController {
         return created;
     }
 
+    @RequestMapping(value="/updateevent", method=RequestMethod.PATCH)
+    public Event updateEvent(@RequestBody Event event) {
+        return eventRepository.save(event);
+    }
+
+    @RequestMapping(value="/removeevent", method=RequestMethod.DELETE)
+    public Event removeEvent(@RequestBody Event event) {
+        eventRepository.delete(event);
+        return event;
+    }
+
+    @RequestMapping(value="/events", method=RequestMethod.GET)
+    public List<Event> getEventsInRange(@RequestParam(value = "start", required = true) String start,
+                                        @RequestParam(value = "end", required = true) String end) {
+        Date startDate = null;
+        Date endDate = null;
+        SimpleDateFormat inputDateFormat=new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            startDate = inputDateFormat.parse(start);
+        } catch (ParseException e) {
+            throw new BadDateFormatException("bad start date: " + start);
+        }
+
+        try {
+            endDate = inputDateFormat.parse(end);
+        } catch (ParseException e) {
+            throw new BadDateFormatException("bad end date: " + end);
+        }
+
+        return eventRepository.findByDatesBetween(startDate, endDate);
+    }
+
     @RequestMapping(value="/specialevent", method=RequestMethod.POST)
     public boolean addEventWithoutDoubleBooking(@RequestBody Event event) {
         Long userId = event.getCalendar().getNjUser().getUserId();
@@ -111,45 +144,6 @@ public class EventController {
     private long getRandomTimeBetweenTwoDates () {
         long diff = endTime - beginTime + 1;
         return beginTime + (long) (Math.random() * diff);
-    }
-
-
-
-
-
-
-
-
-    @RequestMapping(value="/event", method=RequestMethod.PATCH)
-    public Event updateEvent(@RequestBody Event event) {
-        return eventRepository.save(event);
-    }
-
-    @RequestMapping(value="/event", method=RequestMethod.DELETE)
-    public void removeEvent(@RequestBody Event event) {
-        eventRepository.delete(event);
-    }
-
-    @RequestMapping(value="/events", method=RequestMethod.GET)
-    public List<Event> getEventsInRange(@RequestParam(value = "start", required = true) String start,
-                                                  @RequestParam(value = "end", required = true) String end) {
-        Date startDate = null;
-        Date endDate = null;
-        SimpleDateFormat inputDateFormat=new SimpleDateFormat("yyyy-MM-dd");
-
-        try {
-            startDate = inputDateFormat.parse(start);
-        } catch (ParseException e) {
-            throw new BadDateFormatException("bad start date: " + start);
-        }
-
-        try {
-            endDate = inputDateFormat.parse(end);
-        } catch (ParseException e) {
-            throw new BadDateFormatException("bad end date: " + end);
-        }
-
-        return eventRepository.findByDatesBetween(startDate, endDate);
     }
 
 }
